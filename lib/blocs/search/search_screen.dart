c48b780d61603ebc840cca0edb652dd76435bb3a
Oscar <om@codezavod.ru>
2019-11-29 17:06:30 +0300

    Working on slivers

--- lib/blocs/search/search_screen.dart
+++ lib/blocs/search/search_screen.dart
@@ -3,6 +3,8 @@ import 'dart:wasm';
 import 'package:app/shared/entity/language.dart';
 import 'package:app/tatar_keyboard/tatar_input.dart';
 import 'package:app/tatar_keyboard/tatar_keyboard_impl.dart';
+import 'package:flutter/gestures.dart';
+import 'package:statusbar/statusbar.dart';
 
 import 'search_builder.dart';
 import 'search_bloc.dart';
@@ -21,56 +23,160 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    var node = FocusNode();
-    var controller = TextEditingController();
-
-    return Scaffold(
-      appBar: SearchAppBar(
-        height: 150,
-        searchBloc: searchBloc,
-      ),
-      body: TatarKeyboard(
-        tatarInputs: [
-          TatarInput(
-            focusNode: node,
-            textEditingController: controller
-          )
-        ],
-        child: Column(
-          children: <Widget>[
-            Center(
-              child: TextField(
-                focusNode: node,
-                controller: controller,
-                keyboardAppearance: Brightness.light,
-                autocorrect: false,
-                ),
-            ),
-          ],
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverPersistentHeader(
+          pinned: true,
+          delegate: SearchPersistentHeaderDelegate(
+            statusBarHeight: MediaQuery.of(context).padding.top,
+            searchBloc: searchBloc
+          ),
         ),
-      )
+        SearchTabBar(),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index){
+            return ListTile(
+              title: Text("text"),
+            );
+          },
+          childCount: 10),
+        )
+      ],
     );
   }
 }
 
-class SearchAppBar extends StatelessWidget implements PreferredSizeWidget{
+class SearchTabBar extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _SearchTabBarState();
+}
 
-  final double height;
-  final SearchBloc searchBloc;
-
-  const SearchAppBar({@required this.height, @required this.searchBloc})
-   : assert(height != null), assert(searchBloc != null);
+class _SearchTabBarState extends State<SearchTabBar> with SingleTickerProviderStateMixin{
 
   @override
   Widget build(BuildContext context) {
+    return TabBar(
+      tabs: <Widget>[
+        Tab(text: "text",),
+        Tab(text: "text",),
+        Tab(text: "text",)
+      ],
+      controller: TabController(
+        length: 3,    
+        vsync: this
+      ),
+    );
+  }
+
+}
+
+
+class SearchPersistentHeaderDelegate extends SliverPersistentHeaderDelegate{
+
+  final SearchBloc searchBloc;
+
+  final double statusBarHeight;
+
+  double searchBarHeight = 56;
+
+  double fromToHeight = 50;
+
+  SearchPersistentHeaderDelegate({@required this.searchBloc, @required this.statusBarHeight});
+
+  _buildLanguageRow(){
     return FromToLanguageRow(
       searchBloc: searchBloc,
     );
   }
-  
-   @override
-  Size get preferredSize => Size.fromHeight(height);
+
+  _buildSearchBar(){
+    return Container(
+      height: 40,
+      padding: EdgeInsets.only(right: 12, left: 12),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Material(
+          shadowColor: Colors.black,
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 4, right: 4),
+                child: Icon(
+                  Icons.search, 
+                  color: Colors.black45
+                ),
+              ),
+              BlocBuilder<SearchBloc, SearchState>(
+                bloc: searchBloc,
+                builder: (context, state){
+                  return Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: state.searchLangage == Language.Russian ? "Поиск" : "Эзләнү",
+                      ),
+                    )
+                  );
+                },
+              )
+            ],
+          )
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    print(shrinkOffset);
+    StatusBar.color(Theme.of(context).primaryColor);
+    var shrinkRatio = shrinkOffset / fromToHeight;
+    shrinkRatio = shrinkRatio > 1 ? 1 : math.pow(shrinkRatio, 0.5);
+    return SafeArea(
+      child: Material(
+        elevation: 4,
+        shadowColor: Colors.black,
+        color: Theme.of(context).primaryColor,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: 12, bottom: 0),
+                child: SizedBox(
+                  height: fromToHeight - 12,
+                  child: Opacity(
+                    opacity: 1-shrinkRatio,
+                    child: _buildLanguageRow()
+                  )
+                )
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: 8, bottom: 12 - shrinkRatio * 4),
+                child: _buildSearchBar()
+              )
+            )
+          ],
+        )
+      )
+    );
+  }
+
+  @override
+  double get maxExtent => statusBarHeight + searchBarHeight + fromToHeight + 10;
+
+  @override
+  double get minExtent => statusBarHeight + searchBarHeight;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
 }
 
 class FromToLanguageRow extends StatefulWidget {
@@ -87,66 +193,51 @@ class FromToLanguageRow extends StatefulWidget {
 
 class _FromToLanguageRow extends State<FromToLanguageRow>{
 
-  ValueNotifier<List<String>> _langs;
-
   @override
   initState(){
-    _langs = ValueNotifier(["Русский", "Татарский"]);
-
-    if(widget.searchBloc.state.searchLangage == Language.Tatar){
-      _langs.value = _langs.value.reversed.toList();
-    }
     super.initState();
   }
 
   _buildLanguageText({@required int index}){
-    return ValueListenableBuilder<List<String>>(
-      valueListenable: _langs,
-      builder: (context, value, child){
-        return Text(value[index]);
+    return BlocBuilder<SearchBloc, SearchState>(
+      bloc: widget.searchBloc,
+      builder: (context, state){
+        var langs = ["Русский", "Татарский"];
+        if (state.searchLangage == Language.Tatar){
+          langs = langs.reversed.toList();
+        }
+        return Text(langs[index],
+          style: DefaultTextStyle.of(context).style.copyWith(
+            color: Colors.white,
+            fontSize: 15
+          ),
+        );
       },
     );
   }
 
-  _onPressed(){
-    _langs.value = _langs.value.reversed.toList();
-    _langs.notifyListeners();
-    widget.searchBloc.add(SearchLanguageChanged());
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 44,
-            child: Row(
-              children:[
-                Expanded(
-                  child: Align(
-                    child: _buildLanguageText(index: 0),
-                    alignment: Alignment.centerRight,
-                  ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children:[
-                    SizedBox(width: 20),
-                    CompareArrowsButton(
-                      onPressed: _onPressed,
-                    ),
-                    SizedBox(width: 20),
-                  ],
-                ),
-                Expanded(
-                  child: _buildLanguageText(index: 1),
-                ),
-              ],
-            )
-          )
-        ],
-      )
+    return Row(
+      children:[
+        Expanded(
+          child: Align(
+            child: _buildLanguageText(index: 0),
+            alignment: Alignment.centerRight,
+          ),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children:[
+            CompareArrowsButton(
+              onPressed: () => widget.searchBloc.add(SearchLanguageChanged()),
+            ),
+          ],
+        ),
+        Expanded(
+          child: _buildLanguageText(index: 1),
+        ),
+      ],
     );
   }
 
@@ -209,17 +300,26 @@ class _CompareArrowsButtonState extends State<CompareArrowsButton> with SingleTi
       animation: _animation,
       builder: (context, child){
         return GestureDetector(
-          child: RotationTransition(
-            turns: _animation,
-            child: Icon(
-              Icons.compare_arrows,
-              color: _isPressed ? Colors.black54 : Color.lerp(Colors.black, Colors.black54, math.pow(1 - _controller.value, 0.2))
+          child: Container(
+            height: double.infinity,
+            width: 60,
+            color: Colors.transparent,
+            child: RotationTransition(
+              turns: _animation,
+              child: Icon(
+                Icons.compare_arrows,
+                color: _isPressed ? Colors.white24 : Color.lerp(Colors.white, Colors.white24, math.pow(1 - _controller.value, 0.2))
+              ),
             ),
           ),
           onPanDown: (e) => this.setState((){
             _isPressed = true;
           }),
           onPanEnd: (e) => this.setState((){
+            _isPressed = false;
+            _onPressed();
+          }),
+          onPanCancel: () => this.setState((){
             _isPressed = false;
             _onPressed();
           }),
